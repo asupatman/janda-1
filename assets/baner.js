ef74fa30fa5cc3cadf2aa201a293f20a4b6ea2b8
asupatman <110145417+asupatman@users.noreply.github.com>
2024-10-24 22:04:36 +0700

    Add files via upload

---
+++ assets/baner.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '28d616e95ce1677a8725f96915121cad',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcpmcreativeformat.com/28d616e95ce1677a8725f96915121cad/invoke.js"></scr' + 'ipt>');
